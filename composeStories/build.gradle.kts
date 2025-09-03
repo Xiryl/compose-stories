c9cc654221b1a41acfbe3eccc2b4decb3eaeb918
Xiryl <fabio@chiarani.it>
2025-09-03 18:06:12 +0200

    add maven ci

--- composeStories/build.gradle.kts
+++ composeStories/build.gradle.kts
@@ -3,6 +3,8 @@ plugins {
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.detekt)
+    id("com.vanniktech.maven.publish")
+    id("com.gradleup.nmcp")
 }
 
 android {
